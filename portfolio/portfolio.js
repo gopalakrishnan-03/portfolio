ff6d25010650b70c9d1666eb6f236815cf27dca9
Gopala Krishnan <130758502+gopalakrishnan-03@users.noreply.github.com>
2023-05-15 14:13:01 +0530

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,5 +1,5 @@
 
-const menu = document.querySelector('#menu');
+const menu = document.querySelector('.menu');
 const header = document.querySelector('header');
 
 menu.addEventListener('click',()=>{
