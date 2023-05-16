4297c523dfd03d2ff30d12cba0aba4c7f2aedf47
Gopala Krishnan <130758502+gopalakrishnan-03@users.noreply.github.com>
2023-05-16 19:23:50 +0530

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -33,7 +33,31 @@ window.onscroll = ()=>{
 };
 
 
+window.addEventListener("scroll", () => {
 
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (i = 0; i < reveals.length; i++) {
+
+    var windowHeight = window.innerHeight;
+
+    var revealTop = reveals[i].getBoundingClientRect().top;
+
+    var revealPoint =150;
+
+    if (revealTop < windowHeight - revealPoint) {
+
+      reveals[i].classList.add("active");
+
+    } else {
+
+      reveals[i].classList.remove("active");
+
+    }
+
+  }
+
+});
 
 
 
